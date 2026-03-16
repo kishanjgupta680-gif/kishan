4d3f006dd4643eb82f5ba64bc4ffce0f7c5cad7c
kishanjgupta680-gif <kishanjgupta680@gmail.com>
2026-03-16 15:04:51 +0530

    Add files via upload

---
+++ stream1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("Streamlit Text Elements")
+st.header("This is a Header")
+st.subheader("This is a Subheader")
+st.text("This is simple text")
+st.markdown("### Markdown Heading")
+st.write("Streamlit can display text, tables and charts.")
